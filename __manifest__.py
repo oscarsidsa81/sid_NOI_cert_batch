d8ae163f69c3a265c0f48193f78a3d850c712ec2
oscarsidsa81 <ovilches@sid-sa.com>
2025-09-26 09:12:40 +0200

    primer commit

--- __manifest__.py
+++ __manifest__.py
@@ -7,12 +7,14 @@
         Detailed description of the module
     ''',
     'category': 'Uncategorized',
-    'author': 'Cybrosys Techno Solutions',
-    'company': 'Cybrosys Techno Solutions',
-    'maintainer': 'Cybrosys Techno Solutions',
-    'depends': ['stock', 'stock_picking_batch', 'documents','oct_certificate_receptions'],
+    'author': 'Suministros Industriales Diversos s.a.',
+    'company': 'Suministros Industriales Diversos s.a.',
+    'maintainer': 'Suministros Industriales Diversos s.a.',
+    'depends': ['stock', 'stock_picking_batch','web' , 'documents', 'oct_certificate_receptions'],
     'data': [
         'views/sid_NOI_cert_batch_views.xml',
+        "reports/report_notice_inspection_templates.xml",
+        "reports/report_notice_inspection_action.xml",
     ],
     'license': 'LGPL-3',
     'installable': True,
